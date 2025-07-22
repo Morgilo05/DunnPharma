a25e8bc20e80ccccf61cf518f525e2c5cd30fbfa
gilo yea <morgilo05@live.com>
2025-07-22 15:14:26 -0600

    .

--- DunnPharmaAPI/DTOs/PedidoDetalleDto.cs
+++ DunnPharmaAPI/DTOs/PedidoDetalleDto.cs
@@ -7,7 +7,7 @@
 
         public int IdDetalle { get; set; }
         public int IdProducto { get; set; }
-        public string NombreProducto { get; set; }
+        public string? NombreProducto { get; set; }
         public int? Piezas { get; set; }
         public decimal? PrecioUnitario { get; set; }
     }
